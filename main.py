fc7e4c1f274ff1f40333c2031b541a820db9b147
Олександр <Pioner322622@gmail.com>
2023-07-29 15:43:28 +0300

    task 2

--- main.py
+++ main.py
@@ -20,3 +20,26 @@
 
 # degree_number(2, 2) -> 2 * degree_number(2, 1) => 4
 # degree_number(2, 1) -> 2
+
+# task 2
+
+# def stars(symbols):
+#     if symbols > 0:
+#         print("*",end="")
+#         stars(symbols -1)
+#
+# try:
+#
+#     numbers_stars = int(input("Enter numbers of stars: "))
+#     stars(numbers_stars)
+#
+# except Exception as e:
+#     print(e)
+
+# stars(3) -> 3 > 0 -> "*" -> stars(symbols -1)
+# stars(2) -> 2 > 0 -> "*" -> stars(symbols -1)
+# stars(1) -> 1 > 0 -> "*" -> stars(symbols -1)
+# stars(0) -> 0 > 0 -> stars(symbols -1)
+# stars(1) -> stars(symbols -1)
+# stars(2) -> stars(symbols -1)
+# stars(3) -> stars(symbols -1) -> ***
